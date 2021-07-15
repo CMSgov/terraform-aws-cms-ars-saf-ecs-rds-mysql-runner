c409ffb44cba4785d500db958f8e4f59f949f956
M. Adam Kendall <adam@truss.works>
2021-07-15 11:59:12 -0400

    simplify roles

--- main.tf
+++ main.tf
@@ -158,28 +158,6 @@ resource "aws_iam_role_policy" "task_role_policy" {
   })
 }
 
-data "aws_iam_policy_document" "ecs_task_execution_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = "${var.task_name}-${var.environment}-ecs-task-execution-role"
-  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-
 #
 # CloudWatch
 #
@@ -221,7 +199,7 @@ resource "aws_ecs_task_definition" "scheduled_task_def" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "1024"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = aws_iam_role.task_role.arn
 
   container_definitions = templatefile("${path.module}/container-definitions.tpl",
     {
